39d787d933c822982dcac285db146e219372947e
Kartoz9 <gluhovski241@gmail.com>
2023-12-13 09:07:57 +0200

    final

--- src/App.js
+++ src/App.js
@@ -15,18 +15,15 @@ function App() {
     const [selectedDay, setSelectedDay] = useState(null);
 
     const searchPressed = () => {
-        // Запрос текущей погоды
         fetch(`${api.base}weather?q=${search}&units=metric&lang=uk&appid=${api.key}`)
             .then((res) => res.json())
             .then((result) => {
                 setWeather(result);
             });
 
-        // Запрос прогноза на 5 дней
         fetch(`${api.base}forecast?q=${search}&units=metric&lang=uk&appid=${api.key}`)
             .then((res) => res.json())
             .then((result) => {
-                // Фильтруем результаты по следующим трем дням, убирая повторяющиеся дни
                 const uniqueDays = result.list.reduce((days, item) => {
                     const forecastDate = new Date(item.dt_txt).toLocaleDateString("uk", {
                         weekday: "short",
@@ -37,7 +34,6 @@ function App() {
                     return days;
                 }, []);
 
-                // Используем уникальные дни для фильтрации по прогнозу
                 const filteredForecast = result.list.filter((item) =>
                     uniqueDays.includes(
                         new Date(item.dt_txt).toLocaleDateString("uk", { weekday: "short" })
@@ -50,7 +46,6 @@ function App() {
     };
 
     const handleDayClick = (day) => {
-        // Устанавливаем выбранный день
         setSelectedDay(day);
     };
 
@@ -60,7 +55,6 @@ function App() {
                 {/* HEADER */}
                 <h1>Погода</h1>
 
-                {/* Search Box - Input + Button */}
                 <div>
                     <input
                         type="text"
@@ -70,7 +64,6 @@ function App() {
                     <button onClick={searchPressed}>Пошук</button>
                 </div>
 
-                {/* If weather is not empty, display weather */}
                 {weather && (
                     <div className="container" style={{ fontWeight: "bold", fontSize: "20px", width: "1000px", height: '500px' }}>
                         <div className="weather-side">
@@ -123,7 +116,7 @@ function App() {
                                                     weekday: "short",
                                                 }) === selectedDay
                                         )
-                                        .slice(0, 1) // Выводим только первый элемент для каждого выбранного дня
+                                        .slice(0, 1) 
                                         .map((item) => (
                                             <div key={item.dt}>
                                                 <p>Температура: {item.main.temp}°C</p>
